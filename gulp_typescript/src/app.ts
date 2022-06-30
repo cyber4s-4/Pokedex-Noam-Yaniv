7f7714d6326de59a288d00c9c628d0ab7c683359
yaniv-cohen <yaniv.fast@gmail.com>
2022-06-30 13:29:04 +0300

    added inpt

--- gulp_typescript/src/app.ts
+++ gulp_typescript/src/app.ts
@@ -1,18 +1,22 @@
 let str =''
 Promise.all([
-  new Promise(resolve => setTimeout(() => resolve(fetch('https://pokeapi.co/api/v2/pokedex/1')
+  new Promise(resolve => resolve(fetch('https://pokeapi.co/api/v2/pokedex/1')
   .then(resolve=>resolve.json())
   .then(data =>{
-    str = data.pokemon_entries[2].pokemon_species.name;
+    str = data.pokemon_entries[5].pokemon_species.name;
+    // str = data.pokemon_entries[5]
+    console.log(str);
+    
     createElement(str);
      return str
-  })), 500)), // 2
+  }
+  ))), // 2
 ]).then(console.log); // 2,4,6 when promises are ready: each promise is contributes an array member
 // let requests = urls.map(url => fetch(url));
 
 function createElement(value:string){
   let h = document.getElementById('mainDiv') as HTMLDivElement;
-  h.innerHTML+= value
+  h.querySelector('.nameDiv')!.innerHTML+= value
 }
 //   let object = fetch('https://pokeapi.co/api/v2/pokemon-species/4')
 //     .then(response => response.json())
