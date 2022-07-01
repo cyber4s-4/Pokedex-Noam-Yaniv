30a2f8b8494290d33dcc9188d6514dd064f527c5
yaniv-cohen <yaniv.fast@gmail.com>
2022-07-01 15:46:38 +0300

    stats and nore type colors

--- Poke_project/src/app.ts
+++ Poke_project/src/app.ts
@@ -1,4 +1,12 @@
 window.onload= ()=>{
+    document.body.innerHTML =`<div id="parent">
+    <div><label>Enter a Pokemon name:<input id="searchInput" type="text"></label>
+      <button id="goButton">Go!</button>
+    </div>
+    <header>
+    </header>
+    <div id="mainDiv">
+    </div>`
     let goButtonElement = document.getElementById('goButton')! as HTMLButtonElement;
     
     goButtonElement.addEventListener('click',()=>{ goButtonCick()});
@@ -10,10 +18,15 @@ function goButtonCick()
     let input = inputElement.value;
     if(input)
     {
+        clearHTML();
         renderData(input.toLocaleLowerCase())
     }
 }
+function clearHTML (){
 
+        // document.body..='';
+    
+}
 
 
 interface DataOfPokemon {
@@ -35,6 +48,9 @@ class PokemonComponent {
     constructor(data: DataOfPokemon, parent: HTMLDivElement) {
         this.data = data
         this.parent = parent
+    }
+    clear(){
+        this.parent.innerHTML ='';
     }
     render() {
         let IdDiv = document.createElement('div') as HTMLDivElement;
@@ -78,8 +94,11 @@ class PokemonComponent {
             console.log(stat.base_stat);
             let statDiv = document.createElement('div') as HTMLDivElement;
             statDiv.className = `${stat.stat.name}`;
-            // statDiv.style.backgroundColor= `rgb( ${stat.base_stat *250},${stat.base_stat *250},${stat.base_stat *250})`
-            statDiv.innerText= stat.stat.name;
+            statDiv.style.backgroundColor= 'red';
+            console.log( "rgb(" + Math.floor(parseInt( stat.base_stat) *250/256) + ","+ Math.floor(parseInt( stat.base_stat) *250/256) +",30)");
+            statDiv.style.backgroundColor=  "rgb(" + Math.floor(parseInt( stat.base_stat) *250/256) + ","+ Math.floor(256-parseInt(stat.base_stat) *250/256) +",30)";
+            statDiv.style.height = Math.max(parseInt(stat.base_stat) ,50)+'px';
+            statDiv.innerHTML= `<span>${stat.stat.name}<br>${stat.base_stat}</span>`;
             statsContainer.appendChild(statDiv);
         })
         this.parent.appendChild(statsContainer);
@@ -119,8 +138,9 @@ async function renderData(clientSearch: string) {
         evolutionNames: await getEvoNames(data.species.url),
         stats: data.stats,        
     }
-    let parentElement = document.getElementById('parent') as HTMLDivElement;
+    let parentElement = document.getElementById('mainDiv') as HTMLDivElement;
     let poke = new PokemonComponent(dataOfPokemon, parentElement)
+    poke.clear()
     poke.render()
 }
 
