14d2d2663de0a7f3e62d8bfed7206b1b7b32ce43
yaniv-cohen <yaniv.fast@gmail.com>
2022-07-02 16:53:10 +0300

    finished search page

--- Poke_project/src/Pokemon.ts
+++ Poke_project/src/Pokemon.ts
@@ -6,6 +6,7 @@ export interface DataOfPokemon {
     types?: string[]
     abillities?: string;
     imgURL?: string;
+    higherQualityImgURL?: string;
     family?: string[];
     evolutionNames: string[]
 
@@ -20,10 +21,10 @@ export class PokemonComponent {
         this.parent = parent
     }
     clear() {
-        console.log(this.parent);
         this.parent.innerHTML = '';
     }
     renderFullInfo() {
+
         let IdDiv = document.createElement('div') as HTMLDivElement;
         IdDiv.innerText = 'Pokemon ID. :' + this.data.id;
         this.parent.appendChild(IdDiv)
@@ -41,61 +42,80 @@ export class PokemonComponent {
         typesContainer.className = 'typesContainer';
         this.data.types?.forEach((type) => {
             let typesDiv = document.createElement('div') as HTMLDivElement;
-            console.log(type);
+            let capitalizedType = type[0].toUpperCase() + type.substring(1);
             typesDiv.innerHTML = `
-            <div class="typeDiv ${type}">${type}</div>
+            <div class="typeDiv ${type}"><span>${capitalizedType}</span></div>
             `
             typesContainer.appendChild(typesDiv);
         })
         this.parent.appendChild(typesContainer);
 
-        //image of pokemon
-        let pokemonImageDiv = document.createElement('div') as HTMLDivElement;
-        pokemonImageDiv.className = 'pokemonImageDiv';
-        pokemonImageDiv.style.backgroundImage = this.data.imgURL!;
-        this.parent.appendChild(pokemonImageDiv);
 
+                //image of pokemon
+                let pokemonImage = document.createElement('img') as HTMLImageElement;
+                pokemonImage.className = 'pokemonImageDiv';
+                console.log(this.data.higherQualityImgURL);
+                
+                // if(this.data.higherQualityImgURL)
+                // {
+                    // pokemonImage.src = `${this.data.higherQualityImgURL}`;
+                    pokemonImage.src = this.data.higherQualityImgURL!;
 
-        console.log(this.data.stats)
+        
+                // }
+                // else 
+                // {
+                //     pokemonImageDiv.style.backgroundImage = this.data.imgURL!;
+        
+                // }
+                this.parent.appendChild(pokemonImage);
+
 
         //stats
         let statsContainer = document.createElement('div') as HTMLDivElement;
         statsContainer.className = 'pokemonStatDiv';
         this.data.stats?.forEach((stat) => {
-            console.log(stat.base_stat);
             let statDiv = document.createElement('div') as HTMLDivElement;
             statDiv.className = `${stat.stat.name}`;
             statDiv.style.backgroundColor = 'red';
-            console.log("rgb(" + Math.floor(parseInt(stat.base_stat) * 250 / 256) + "," + Math.floor(parseInt(stat.base_stat) * 250 / 256) + ",30)");
-            statDiv.style.backgroundColor = "rgb(" + Math.floor(parseInt(stat.base_stat) * 250 / 256) + "," + Math.floor(256 - parseInt(stat.base_stat) * 250 / 256) + ",30)";
+            statDiv.style.backgroundColor =
+                "rgb(" + (20 + Math.floor(parseInt(stat.base_stat) * 250 / 256)) + ","
+                +  (20 + Math.floor(255- parseInt(stat.base_stat) * 250 / 256))
+                + ",30)";
             statDiv.style.height = Math.max(parseInt(stat.base_stat), 50) + 'px';
             statDiv.innerHTML = `<span>${stat.stat.name}<br>${stat.base_stat}</span>`;
             statsContainer.appendChild(statDiv);
         })
         this.parent.appendChild(statsContainer);
+
+        console.log('evonames ' + this.data.evolutionNames);
+        //evo
+        let evoDiv = document.createElement('div') as HTMLDivElement;
+        evoDiv.innerText = 'Pokemon evolutions: ' + this.data.evolutionNames;
+        this.parent.appendChild(evoDiv);
     }
-    renderMinInfo() {
-        this.parent.className = 'gallery'
-        let pokeBlock = document.createElement('div') as HTMLDivElement;
-        pokeBlock.classList.add('poke-block')
-        
-        let pokeImg = document.createElement('div') as HTMLDivElement;
-        pokeImg.className = 'poke-little-img';
-        pokeImg.style.backgroundImage = this.data.imgURL!;
-        
-        let pokeName = document.createElement('div') as HTMLDivElement;
-        pokeName.innerHTML = `Name: ${this.data.name!} <br>ID: ${this.data.id!}`
-        pokeImg.appendChild(pokeName)
-        
-        pokeBlock.addEventListener('click', () => {
-           window.location.href = `http://localhost:4000/?pokemon=${this.data.id}`;
-        })
-        pokeBlock.appendChild(pokeImg)
-        this.parent.appendChild(pokeBlock)
-        /* TODO :
-            change this.parent class name to gallery, gallery will have space around flex
-            create pokeblock that contains img, name and id.
-            add pokeblock listener of click that change location to ?pokemon=${poke.id}
-        */
-    }
+    // renderMiniInfo() {
+    //     this.parent.className = 'gallery'
+    //     let pokeBlock = document.createElement('div') as HTMLDivElement;
+    //     pokeBlock.classList.add('poke-block')
+
+    //     let pokeImg = document.createElement('div') as HTMLDivElement;
+    //     pokeImg.className = 'poke-little-img';
+    //     pokeImg.style.backgroundImage = this.data.imgURL!;
+    //     pokeBlock.appendChild(pokeImg)
+
+    //     let pokeName = document.createElement('div') as HTMLDivElement;
+    //     pokeName.innerHTML = `Name: ${this.data.name!} <br>ID: ${this.data.id!}`
+    //     pokeImg.appendChild(pokeName)
+
+    //     pokeBlock.addEventListener('click', () => {
+    //        window.location.href = `http://localhost:4000/?pokemon=${this.data.id}`;
+    //     })
+    //     this.parent.appendChild(pokeBlock)
+    //     /* TODO :
+    //         change this.parent class name to gallery, gallery will have space around flex
+    //         create pokeblock that contains img, name and id.
+    //         add pokeblock listener of click that change location to ?pokemon=${poke.id}
+    //     */
+    // }
 }
--- Poke_project/src/app.ts
+++ Poke_project/src/app.ts
@@ -21,60 +21,224 @@ async function renderData(clientSearch: string, parentElement: HTMLDivElement) {
         height: data.height,
         weight: data.weight,
         types: data.types.map((type: { type: { name: any; }; }) => type.type.name),
-        imgURL: `url('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png')`,
+        imgURL: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`,
+        higherQualityImgURL: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`,
         evolutionNames: await getEvoNames(data.species.url),
         stats: data.stats,
     }
     let poke = new PokemonComponent(dataOfPokemon, parentElement)
     if (app.checkPokemon === true)
         poke.renderFullInfo()
-    else
-        poke.renderMinInfo()
+    // else
+    //     {poke.renderMiniInfo()}
 }
 class App {
     public checkPokemon: boolean = location.search.includes('pokemon');
-    mainSetUp() {
-        let searchBar = document.createElement('div') as HTMLDivElement;
-        searchBar.innerHTML = "Enter a Pokemon name or ID:"
+
+    async mainSetUp() {
+        function loadMorePokemon(limitOfPokemons: number, parentElement: HTMLDivElement): number {
+            console.log('loadMorePokemon');
+
+            let index = limitOfPokemons
+            limitOfPokemons += 10;
+            while (index < limitOfPokemons && localMiniData.length > 0) {
+                let element = localMiniData.shift() as { pokemon_species: { name: string, url: string }, entry_number: string };
+                renderMiniInfo(element, parentElement)
+                index++
+            }
+            // while (index < limitOfPokemons && index<localMiniData.length) {
+            //     let element = localMiniData[index] as { pokemon_species: { name: string, url: string }, entry_number: string };
+            //     renderMiniInfo(element, parentElement)
+            //     index++
+            // }
+            return limitOfPokemons;
+        }
+
+
+
+        //save all pokemon entry numbers and names
+        let miniData: Array<any> = await getMiniData();
+        let localMiniData: Array<any> = miniData;
+
+        console.log(miniData);
+
+        //set up searchbar
+        let searchBarDiv = document.createElement('div') as HTMLDivElement;
+        searchBarDiv.classList.add('searchBarDiv');
+
+        let searchBarTextDiv = document.createElement('div') as HTMLDivElement;
+        searchBarTextDiv.classList.add('searchBarText');
+        let searchBarH2 = document.createElement('h2')! as HTMLHeadingElement
+        searchBarH2.innerText = "Enter a Pokemon name or ID:"
+        searchBarTextDiv.appendChild(searchBarH2)
         let inputElement = document.createElement('input')! as HTMLInputElement
-        searchBar.appendChild(inputElement)
+        searchBarTextDiv.appendChild(inputElement)
+        searchBarDiv.appendChild(searchBarTextDiv);
+
         let goButtonElement = document.createElement('button')! as HTMLButtonElement;
         goButtonElement.innerHTML = "Go!"
         goButtonElement.addEventListener('click', () => {
-            window.location.href = `http://localhost:4000/?pokemon=${inputElement.value}`;
-        });
-        searchBar.appendChild(goButtonElement)
-        document.body.appendChild(searchBar)
 
+            let pokemonCount = 0;
+            let searchValue = '';
+            parentElement.innerHTML = '';
+            localMiniData = [];
+
+            for (const entry of miniData) {
+                if ((entry.pokemon_species.name.includes(inputElement.value) || entry.entry_number.toString().startsWith('' + inputElement.value))) {
+                    console.log((entry.pokemon_species.name.includes(inputElement.value) || entry.entry_number.toString().startsWith('' + inputElement.value)));
+                    console.log(entry);
+                    pokemonCount++;
+                    searchValue = entry.pokemon_species.name;
+                    localMiniData.push(entry);
+                }
+
+            }
+            if (pokemonCount == 0) {
+                console.log('no pokemon with that information');
+            }
+            if (pokemonCount == 1) {
+                console.log(searchValue);
+
+                window.location.href = `http://localhost:4000/?pokemon=${searchValue}`;
+            }
+            else if (pokemonCount > 1) {
+
+                while (localMiniData.length > 0) {
+                    renderMiniInfo(localMiniData.shift(), parentElement);
+                }
+            }
+        });
+        searchBarDiv.appendChild(goButtonElement)
+        document.body.appendChild(searchBarDiv)
+
+        //insert pokemon DOM Elements
         let parentElement = document.createElement('div') as HTMLDivElement;
         document.body.appendChild(parentElement)
-        let i = 1;
-        let limitOfPokemons = 10;
-        while (i < limitOfPokemons) {
-            renderData(i.toString(), parentElement)
-            i++
-        }
-        let loadMore = document.createElement('button') as HTMLButtonElement;
-        loadMore.innerHTML = 'Load More Pokemons!'
-        loadMore.addEventListener('click', () => {
-            limitOfPokemons += 10;
-            while (i < limitOfPokemons) {
-                renderData(i.toString(), parentElement)
-                i++
-            }
-        })
-        document.body.appendChild(loadMore)
 
+
+        let limitOfPokemons = 10;
+        let index = 0
+        for (; index < limitOfPokemons/*miniData.length*/; index++) {
+            let element = miniData[index] as { pokemon_species: { name: string, url: string }, entry_number: string };
+            console.log(element);
+            renderMiniInfo(element, parentElement)
+            //image url `url('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png')`
+        }
+
+        //load more data button 
+        // let loadMore = document.createElement('button') as HTMLButtonElement;
+        // loadMore.innerHTML = 'Load More Pokemons!'
+        // loadMore.addEventListener('click', () => {
+        //     limitOfPokemons = loadMorePokemon(limitOfPokemons, parentElement);
+
+        // })
+        // document.body.appendChild(loadMore)
+        //detect end of page
+        window.onscroll = function () {
+            if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+                //The load time is artificial LOL
+                setTimeout(() => { limitOfPokemons = loadMorePokemon(limitOfPokemons, parentElement) }, 200);
+            }
+        }
     }
     pokeSetUp(pokemonID: string) {
         let parentElement = document.createElement('div') as HTMLDivElement;
         document.body.appendChild(parentElement)
         renderData(pokemonID, parentElement)
+
+
+
+    }
+    //for the header, nav and footer
+    basePageSetUp() {
+        //set up the header
+        console.log('basePageSetUp');
+        let body = document.getElementsByTagName('body')[0] as HTMLBodyElement;
+        let header = document.createElement('header') as HTMLElement;
+
+        let logo = document.createElement('img') as HTMLImageElement;
+        logo.classList.add('logo');
+        logo.src = 'https://assets.pokemon.com/assets/cms2/img/misc/gus/buttons/logo-pokemon-79x45.png';
+        header.appendChild(logo)
+        let shop = document.createElement('img') as HTMLImageElement;
+        shop.classList.add('shop');
+        shop.src = 'https://assets.pokemon.com/assets/cms2/img/misc/gus/buttons/logo-pokemoncenter-79x45.png'
+        header.appendChild(shop)
+        body.appendChild(header)
+
+        //set up the nav
+        console.log('basePageSetUp');
+        let nav = document.createElement('nav') as HTMLElement;
+
+        //home button
+        let home = document.createElement('div') as HTMLDivElement;
+        home.classList.add('home');
+        home.innerText = 'Home'
+        nav.appendChild(home)
+        //pokedex button
+        let pokedex = document.createElement('div') as HTMLDivElement;
+        pokedex.classList.add('pokedex');
+        pokedex.innerText = 'pokedex';
+        pokedex.addEventListener('click', () =>{
+            window.location.href = `http://localhost:4000/`;
+    
+            })
+        nav.appendChild(pokedex)
+        //VIdeo Games & Apps button
+        let video_games = document.createElement('div') as HTMLDivElement;
+        video_games.classList.add('video_games');
+        video_games.innerText = 'video_games'
+        nav.appendChild(video_games)
+
+        body.appendChild(nav)
     }
 }
 let app = new App()
+app.basePageSetUp();
 if (app.checkPokemon === true) {
     app.pokeSetUp((new URLSearchParams(location.search)).get('pokemon')!)
 } else {
     app.mainSetUp()
+}
+
+
+
+async function getMiniData() {
+    console.log('getMiniData');
+    let response = await fetch('https://pokeapi.co/api/v2/pokedex/1');
+    let data = (await response.json()).pokemon_entries;
+    return data
+}
+
+function renderMiniInfo(pokemon: { pokemon_species: { name: string, url: string }, entry_number: string },
+    parent: HTMLDivElement) {
+    parent.classList.add('pokemonContainer')
+
+    let pokeDiv = document.createElement('div') as HTMLDivElement;
+    pokeDiv.classList.add('pokemonBox');
+    pokeDiv.addEventListener('click', () => {
+        window.location.href = `http://localhost:4000/?pokemon=${pokemon.entry_number}`;
+    })
+    //create pokemon image DOM element
+    let imgElement = document.createElement('img') as HTMLImageElement
+    imgElement.classList.add('pokemonImageDiv');
+    imgElement.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.entry_number}.png`
+
+    pokeDiv.appendChild(imgElement)
+
+    //create pokemon entry_number DOM element
+    let entryDiv = document.createElement('h3') as HTMLHeadingElement
+    //add zeros to the number
+    console.log('zeros : ' + '0'.repeat(3 - pokemon.entry_number.toString().length) + ' for ' + pokemon.entry_number);
+    let numberString = '#' + '0'.repeat(3 - pokemon.entry_number.toString().length) + pokemon.entry_number
+    entryDiv.innerText = numberString;
+    pokeDiv.appendChild(entryDiv)
+
+    //create pokemon name DOM element
+    let nameDiv = document.createElement('h2') as HTMLHeadingElement
+    nameDiv.innerText = pokemon.pokemon_species.name
+    pokeDiv.appendChild(nameDiv)
+
+    parent.appendChild(pokeDiv)
 }
