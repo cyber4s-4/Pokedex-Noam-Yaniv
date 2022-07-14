6624ef7e9a525d7774ae7d706847f1d2e7ddc3fa
noambenshoham <noambenshoham@cyber4s.dev>
2022-07-14 13:56:46 +0300

    try2

---
+++ src/server/server.ts
@@ -0,0 +1,41 @@
+import path from "path";
+import express, { Express } from "express";
+import cors from "cors";
+import { json } from "body-parser";
+import { Server } from "http";
+// @ts-ignore
+const { WebSocketServer, WebSocket, Server } = require("ws");
+const fs = require("fs");
+
+const app: Express = express();
+app.use(cors());
+app.use(json());
+const root: string = path.join(__dirname, "dist");
+
+app.use(express.static(root));
+
+app.get("/", (_req, res) => {
+  res.status(200).sendFile(path.join(root, "index.html"));
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log("Hosted: http://localhost:" + port);
+});
+
+const wss: Server = new Server({ server: app });
+// @ts-ignore
+wss.on("connention", (client) => {
+  // @ts-ignore
+  client.on("message", (msg) => {
+    if (msg.toString() === "get-pokemons") {
+      let allPokemons = JSON.parse(fs.readFileSync("../pokemonsObj.json"));
+      client.send(JSON.stringify(allPokemons));
+    }
+  });
+
+  client.on("close", () => {
+    console.log("Websocket disconnected");
+  });
+});
+console.log("Websocket: ws://localhost:" + 4040);
